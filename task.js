f6bf51dc737495cabdaa41cec6b18060987afa82
mamume <mmustafametwally@gmail.com>
2021-08-27 18:26:24 +0200

    Second Edit From Person 2 (Mahmoud Metwally)

--- task.js
+++ task.js
@@ -1,7 +1,10 @@
 /* this is line 1 */
 
-// Edit From person 2
+// First Edit From person 2 (Mahmoud Metwally)
 console.log('Hello, World!')
 
 
 // First edit from person 1 
+
+// Second Edit From Person 2 (Mahmoud Metwally)
+console.log('Second Edit')
